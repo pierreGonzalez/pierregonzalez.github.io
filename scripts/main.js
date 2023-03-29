3e7ef21f4ebc0e005d5c15be904c3f6817bf6736
Pierre Gonzalez <pierre.gonzalez@me.com>
2023-03-29 21:46:27 +0200

    update with pdf
    added resume in pdf

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,14 @@ AOS.init({
 });
 
 // Add your javascript here
+function getMyAge(){
+  var dob = new Date("09/08/1987");  
+  var diff_ms = Date.now() - dob.getTime();
+  var age_dt = new Date(diff_ms); 
+  let myAge = Math.abs(age_dt.getUTCFullYear() - 1970);
+
+  document.getElementById("myAge").innerHTML = myAge;
+}
+
+window.onload = getMyAge()
+ 
